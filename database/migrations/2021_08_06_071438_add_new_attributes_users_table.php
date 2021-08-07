48bcb5b483f9f18379913b3393c723a9292b0293
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-08-06 17:15:50 -0700

    Initial Commit

---
+++ database/migrations/2021_08_06_071438_add_new_attributes_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNewAttributesUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *php artisan make:migration add_new_attributes_users_table
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('web_site');
+            $table->text('presentation');
+            $table->boolean('status')->default(0);
+            $table->string('nick_name');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('web_site')->nullable();
+            $table->dropColumn('presentation')->nullable();
+            $table->dropColumn('status')->default(0);
+            $table->dropColumn('nick_name')->unique();
+        });
+    }
+}
